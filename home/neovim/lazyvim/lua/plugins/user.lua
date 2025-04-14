d0f2146d0782655ab9cd580e16acf077c818a736
aldouslpzchr <elpizochari.aldous@moneyforward.co.jp>
2025-04-14 13:44:29 +0900

    update

--- home/neovim/lazyvim/lua/plugins/user.lua
+++ home/neovim/lazyvim/lua/plugins/user.lua
@@ -12,4 +12,58 @@ return {
       },
     },
   },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        enabled = true,
+        actions = {
+          confirm_and_close = function(picker)
+            picker:action("confirm")
+            picker:action("close")
+          end,
+          confirm_nofocus = function(picker)
+            picker:action("confirm")
+            picker:focus()
+          end,
+        },
+        sources = {
+          explorer = {
+            auto_close = true,
+            win = {
+              list = {
+                keys = {
+                  ["L"] = "confirm_and_close",
+                  ["l"] = "confirm_nofocus",
+                },
+              },
+            },
+            layout = {
+              cycle = true,
+              layout = {
+                box = "horizontal",
+                position = "float",
+                height = 0.7,
+                width = 0.7,
+                border = "rounded",
+                {
+                  box = "vertical",
+                  width = 40,
+                  min_width = 40,
+                  { win = "input", height = 1, title = "{title} {live} {flags}", border = "single" },
+                  { win = "list" },
+                },
+                { win = "preview", width = 0, border = "left" },
+              },
+            },
+          },
+        },
+      },
+      terminal = {
+        win = {
+          position = "float",
+        },
+      },
+    },
+  },
 }
